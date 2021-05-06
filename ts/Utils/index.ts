161bf251023e913835b1ccc28f83968b7886e18e
zhangtao <zhangtao@agora.io>
2021-05-06 15:32:54 +0800

    update ts tag message

--- ts/Utils/index.ts
+++ ts/Utils/index.ts
@@ -5,11 +5,11 @@
  * @Last Modified time: 2021-04-28 13:34:31 
  */
 
-export const deprecate = (originApi?: string, replaceApi?: string) => {
-  console.warn(`This method ${originApi} will be deprecated soon. `, replaceApi ? `Please use ${replaceApi} instead` : '');
-};
-
 export const TAG = "[Agora]: "
+
+export const deprecate = (originApi?: string, replaceApi?: string) => {
+  console.warn(`${TAG} This method ${originApi} will be deprecated soon. `, replaceApi ? `Please use ${replaceApi} instead` : '');
+};
 
 export const logWarn = (msg: string, tag: string = TAG) => {
   console.warn(`${tag} ${msg}`)
